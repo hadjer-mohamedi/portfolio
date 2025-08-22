48c9133df7c8a977ab3076ae2f63e6b36a2cf3cd
unknown <MANAGER@125PC400.bna.lab>
2025-08-22 21:57:55 +0100

    updating code

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // If deploying to https://<user>.github.io/<repo>/ set base: '/<repo>/'.
 export default defineConfig({
   plugins: [react()],
-  base: '', // change to '/hadjer-portfolio/' if deploying to a project page
+  base: '/portfolio/', // change to '/hadjer-portfolio/' if deploying to a project page
 })
